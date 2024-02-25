f1dfcce326df2469983de2c8542462400ad2130f
youssef-nachar <156086960+youssef-nachar@users.noreply.github.com>
2024-02-25 14:46:56 +0200

    Add files via upload

---
+++ mygame.js
@@ -0,0 +1,25 @@
+var hero =document.getElementById("hero");
+var obstacle = document.getElementById("obstacle");
+function jump(){
+    hero.classList.add("animate");
+    setTimeout(function(){
+    hero.classList.remove("animate");
+    },2000);
+}
+
+var checkStatus = setInterval(function(){
+    var heroTop =parseInt( window.getComputedStyle(hero).getPropertyValue("top"));
+    var obstacleLeft=parseInt(window.getComputedStyle(obstacle).getPropertyValue("left"));
+    if(obstacleLeft<30 && obstacleLeft>0 && heroTop>=200 ){
+        // obstacle.style.animation="none";
+        // obstacle.style.display="none";
+         alert("you lose");
+    }
+},10);
+
+
+var retry = document.getElementById("retry");
+
+function retry(){
+    retry.classList.remove("retry")
+}
