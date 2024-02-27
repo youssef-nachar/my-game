3b15df49e657eff84bdbcd84a86132a6d3a958fc
youssef-nachar <156086960+youssef-nachar@users.noreply.github.com>
2024-02-27 17:02:22 +0200

    Update mygame.js

--- mygame.js
+++ mygame.js
@@ -1,43 +1,46 @@
-var hero =document.getElementById("hero");
+var hero = document.getElementById("hero");
 var obstacle = document.getElementById("obstacle");
 var counter = document.getElementById("counter");
-var you_win=document.getElementById("you_win");
-var continu=document.getElementById("continue");
+var you_win = document.getElementById("you_win");
+var continu = document.getElementById("continue");
 var t =0;
 function jump(){
     hero.classList.add("animate");
     setTimeout(function(){
     hero.classList.remove("animate");
     },1000);
+    // if(obstacleLeft < 30 &&heroTop >= 200 ){
     t++;
-    counter.innerHTML=t;
-    if(t == 3) { 
-        obstacle.style.animation= "paused";
+    counter.innerHTML ="Score:"+ t;
+    var text=document.getElementById("text")
+    if(t == 10) { 
+        // you_win.innerHTML="you good"
+        obstacle.style.animationPlayState="paused";
         you_win.style.visibility="visible";
     }
+ if(t == 15){ 
+    you_win.style.visibility="visible";
+    text.innerHTML="you are a special";
+ }
 }
-continu=()=>{   
-    you_win.style.display = "none";
-    obstacle.style.animation="running";
-    
-}
+
+
+// ====================== the lose
+var lose = document.getElementById("lose_container");
 var checkStatus = setInterval(function(){
     var heroTop =parseInt( window.getComputedStyle(hero).getPropertyValue("top"));
     var obstacleLeft=parseInt(window.getComputedStyle(obstacle).getPropertyValue("left"));
     if(obstacleLeft<30 && obstacleLeft>0 && heroTop >= 200 ){
-        // obstacle.style.animation="none";
-        // obstacle.style.display="none";
-         document.write("you lose");
+        lose.style.visibility="visible";
     }
 },10);
 
-// var retry = document.getElementById("retry");
-// var play = document.getElementById("play");
-// click=0;
-// function play(){
-//     if(click == 1){
-//             retry.style.display="none";
-//         }
-          
-// }
-
+//=====================continue button====================== 
+continu=()=>{   
+    obstacle.style.animationPlayState="running"
+    you_win.style.display = "none";
+}
+retry=()=>{
+    lose.style.visibility="hidden"; 
+    t=0
+}
